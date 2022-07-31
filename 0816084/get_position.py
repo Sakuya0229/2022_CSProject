c8e543d243495da05566e97b15c6cfcdeca4dce2
brian1357929 <brian1357929@gmail.com>
2022-07-31 21:57:01 +0800

    20220731

--- 0816084/get_position.py
+++ 0816084/get_position.py
@@ -8,7 +8,7 @@ pts1 = np.float32([[94,775],[442,669],[859,1033],[1094,754]])
 pts2 = np.float32([[307,80 ],[564,80],[307,468],[564,468]])
 M = cv2.getPerspectiveTransform(pts1,pts2)
 
-
+badminton_haar = cv2.CascadeClassifier(r"cascade.xml")
 
 hog = cv2.HOGDescriptor()
 hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
@@ -35,6 +35,10 @@ while(capR.isOpened()):
     # img = hisEqulColor(img)
     if flag%5 ==0:
         rectsR, weightsR = hog.detectMultiScale(imgR,winStride=(12, 8), scale=1.05,useMeanshiftGrouping = False)
+    gray = cv2.cvtColor(imgR, cv2.COLOR_BGR2GRAY)
+    badminton = badminton_haar.detectMultiScale(gray, 1.06, 1,minSize=(20, 20))
+
+
 
     for body_i in rectsR:
         (x, y, w, h) = body_i
@@ -44,6 +48,8 @@ while(capR.isOpened()):
             #point=np.array([x+w/2,y+h-10])
             pos=cv2.perspectiveTransform(point,M)[0][0]
             im = cv2.putText(imgR, f"{round(pos[0],3),round(pos[1],3)}", (x, y), cv2.FONT_HERSHEY_PLAIN, 1.5, (0, 0, 255), 1, cv2.LINE_AA)
+    for (x, y, w, h) in badminton:
+        cv2.rectangle(imgR, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
     flag +=1
     # cv2.imshow("result",imgL)
